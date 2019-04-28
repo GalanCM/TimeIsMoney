4625d66c84dc79daecf86a628039a95a18634d5b
Galan Montgomery <galan.c.m@gmail.com>
2019-04-28 19:21:06 -0400

    Small text tweak.

--- Reward.gd
+++ Reward.gd
@@ -1,6 +1,6 @@
 extends CenterContainer
 
 func announce(name):
-	$Label.text = "As a thanks for the players' hard work\n A reward:  " + name
+	$Label.text = "As a thanks for the players' hard work\n We bought this for you: " + name +"!"
 	$AnimationPlayer.play("Unveil")
 	$AudioStreamPlayer.play()
